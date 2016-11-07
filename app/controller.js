4505d3ecec1246e279c7ab9614286185ca219702
Michael Thomas <mtzeus96@gmail.com>
2016-11-06 20:21:20 -0500

    made controller much thinner

--- app/controller.js
+++ app/controller.js
@@ -1,59 +1,8 @@
 app.controller('MainController', ['$scope', "applyFilters", "getResults", function ($scope, applyFilters, getResults) {
-  $scope.apply = applyFilters;
-  $scope.minPrice = 0;
-  $scope.maxPrice = 10000000000;
-  $scope.bathrooms = 0;
-  $scope.bedrooms = 0;
-  $scope.squareFeet = 0;
-  $scope.homeType = 0;
+
+  $scope.apply = applyFilters.initResults;
+  $scope.apply();
+  $scope.results = applyFilters.results;
 
 
-  $scope.initResults = function () {
-    getResults.success(function (data) {
-
-      $scope.results = [];
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-          lat: 45.397,
-          lng: -70.644
-        },
-        zoom: 5
-      });
-
-
-      for (var i = 0; i < data.homes.length; i++) {
-        var dataBeds = parseFloat(data.homes[i].beds);
-        var dataBaths = parseFloat(data.homes[i].baths);
-        var dataPrice = parseFloat(data.homes[i].price);
-        var dataSqft = parseFloat(data.homes[i].sqft);
-        var lat = data.homes[i].lat;
-        var long = data.homes[i].long;
-        var address = data.homes[i].address;
-        var myLatLng = {
-          lat: parseInt(data.homes[i].lat),
-          lng: parseInt(data.homes[i].long)
-        };
-
-
-        if ((dataBaths >= $scope.bathrooms) && (dataBeds >= $scope.bedrooms) && ($scope.minPrice <= dataPrice) && (dataPrice <= $scope.maxPrice) && (dataSqft >= $scope.squareFeet)) {
-          var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: 'Hello World!'
-          });
-          $scope.results.push(data.homes[i]);
-
-        }
-
-      }
-    });
-  };
-  $scope.initResults();
-
-
-
-
-
-
-
-      }]);
+}]);
--- app/services.js
+++ app/services.js
@@ -8,10 +8,65 @@ app.factory('getResults', ['$http', function ($http) {
       return err;
     });
 }]);
-app.factory('applyFilters', ['$http', function ($http) {
-  return function (item) {
+app.factory('applyFilters', ['getResults', function (getResults) {
 
-
+  var minPrice = 0;
+  var maxPrice = 10000000000;
+  var bathrooms = 0;
+  var bedrooms = 0;
+  var squareFeet = 0;
+  var homeType = 0;
+  var results = {
+    homes: []
   };
+
+  return {
+    initResults: function () {
+      getResults.success(function (data) {
+
+        results.homes = [];
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {
+            lat: 45.397,
+            lng: -70.644
+          },
+          zoom: 5
+        });
+
+
+        for (var i = 0; i < data.homes.length; i++) {
+          var dataBeds = parseFloat(data.homes[i].beds);
+          var dataBaths = parseFloat(data.homes[i].baths);
+          var dataPrice = parseFloat(data.homes[i].price);
+          var dataSqft = parseFloat(data.homes[i].sqft);
+          var lat = data.homes[i].lat;
+          var long = data.homes[i].long;
+          var address = data.homes[i].address;
+          var myLatLng = {
+            lat: parseInt(data.homes[i].lat),
+            lng: parseInt(data.homes[i].long)
+          };
+
+
+          if ((dataBaths >= bathrooms) && (dataBeds >= bedrooms) && (minPrice <= dataPrice) && (dataPrice <= maxPrice) && (dataSqft >= squareFeet)) {
+            var marker = new google.maps.Marker({
+              position: myLatLng,
+              map: map,
+              title: 'Hello World!'
+            });
+            results.homes.push(data.homes[i]);
+
+          }
+        }
+      });
+    },
+    results: results
+
+  }
+
+
+
+
+
 
 }]);
