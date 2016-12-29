93da875198c4d0bd1087ed3d7c3bc9fe0b745f6d
Michael Thomas <mtzeus96@gmail.com>
2016-12-29 00:15:05 -0500

    the style sheets are much better

--- app/module.js
+++ app/module.js
@@ -5,37 +5,7 @@ app.directive("city", function () {
     templateUrl: "results.html"
   };
 });
-app.directive('head', ['$rootScope', '$compile', "applyFilters",
-    function ($rootScope, $compile, applyFilters) {
-    return {
-      restrict: 'E',
-      link: function (scope, elem) {
-        var html = '<link rel="stylesheet" ng-repeat="(routeCtrl, cssUrl) in routeStyles" ng-href="{{cssUrl}}" />';
-        elem.append($compile(html)(scope));
-        scope.routeStyles = {};
-        $rootScope.$on('$routeChangeStart', function (e, next, current) {
-          if (current && current.$$route && current.$$route.css) {
-            if (!angular.isArray(current.$$route.css)) {
-              current.$$route.css = [current.$$route.css];
-            }
-            angular.forEach(current.$$route.css, function (sheet) {
-              delete scope.routeStyles[sheet];
-            });
-          }
-          if (next && next.$$route && next.$$route.css) {
-            if (!angular.isArray(next.$$route.css)) {
-              next.$$route.css = [next.$$route.css];
-            }
-            angular.forEach(next.$$route.css, function (sheet) {
-              scope.routeStyles[sheet] = sheet;
-            });
-          }
-          applyFilters.initResults();
-        });
-      }
-    };
-    }
-]);
+
 
 
 app.config(['$routeProvider', function ($routeProvider) {
